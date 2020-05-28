423cecfae72b1618408acb639ec2aae441eeda73
Kirill <constontinovih@gmail.com>
2020-05-28 09:00:57 +0300

    1 homework

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 0);
+
+movie = new Object;
+actor = new Object;
+genre = {};
+PersonalMovieBD = {
+    count: numberOfFilms,
+    movies: { },
+    actors: { },
+    genres: [ ],
+    privat: false
+}
+ 
+for (i = 0; i<2; i++){
+    a = prompt ('Один из последних просмотренных фтльмов?');
+    b = prompt ('На сколько оцените его');
+    PersonalMovieBD.movies[a] = b;
+}
+console.log (PersonalMovieBD.movies)

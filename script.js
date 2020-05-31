c1db44ee58e2929d6fd1b32ef179f7e17491c435
Kirill <constontinovih@gmail.com>
2020-05-31 15:13:28 +0300

    optimised and fix

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 0);
+function promt () {
+    numberOfFilms = Number(prompt("Сколько фильмов вы уже посмотрели?", ""));
+    if ((numberOfFilms.length == 0) || (numberOfFilms.length >= 50)) {
+        promt()
+    }
+}
 
-movie = new Object;
-actor = new Object;
-genre = {};
+promt();
+
 PersonalMovieBD = {
     count: numberOfFilms,
     movies: { },
@@ -10,10 +14,28 @@ PersonalMovieBD = {
     genres: [ ],
     privat: false
 }
- 
+
+ if (PersonalMovieBD.count < 10){
+     alert ("Просмотренно довольно мало фильмов")
+ } else if ((PersonalMovieBD.count >= 10) || (PersonalMovieBD.count < 30)) {
+     alert ('Вы классический зритель')
+ } else if (PersonalMovieBD.count > 30) {
+     alert ("Вы киноман")
+ } else {
+     console.log ("Произошла ошибка")
+ }
+
+function promt_2 (){
+    a = prompt ('Один из последних просмотренных фтльмов?', '');
+    b = prompt ('На сколько оцените его', '');
+    if ((a.length == 0) || (a.length > 50)) {
+        promt_2
+    }
+    return (a,b)
+}
 for (i = 0; i<2; i++){
-    a = prompt ('Один из последних просмотренных фтльмов?');
-    b = prompt ('На сколько оцените его');
+    promt_2 ()
     PersonalMovieBD.movies[a] = b;
 }
+
 console.log (PersonalMovieBD)
